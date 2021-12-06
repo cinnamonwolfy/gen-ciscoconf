5408b59bdbca4a6642c1c7c9664937375c69fc8c
pocketlinux32 <pocketlinux32@gmail.com>
2021-12-05 22:29:29 -0600

    cleanup and add

--- src/cisco-types.c
+++ src/cisco-types.c
@@ -10,14 +10,16 @@
 // Cisco Interface Structure
 struct ciscoint {
 	char* ports;			// Interface(s)
-	char* mode;			// Mode of the interface(s)
+	int mode;			// Mode of the interface(s)
 	char* allowed_vlans;		// VLANS allowed to cross the trunk. Only used if mode = "trunk"
 	char* ip_addr;			/* IP address of the interface (VLANs for switches, everything else for routers). IP version
 					automatically determined.*/
-	char* sub_mask;			// Subnet mask for IP address. CIDR preferred but not required
+	int sub_mask;			// Subnet mask for IP address. CIDR preferred but not required
 	char* gateway;			// Gateway for interface. Only used if ports = "vlan x"
 }
 
 // Cisco Table Structure
 
-struct cisco
+struct ciscotable {
+	
+}
