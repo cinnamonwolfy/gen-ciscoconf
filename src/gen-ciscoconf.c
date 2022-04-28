413e38a405738989330722c0f785000d364fd174
pocketlinux32 <pocketlinux32@gmail.com>
2022-04-27 22:45:46 -0500

    yes

---
+++ src/gen-ciscoconf.c
@@ -0,0 +1,12 @@
+#include <ciscolib-const.h>
+#include <ciscolib.h>
+
+uint8_t commandParser(plarray_t* args, plgc_t* gc){
+	char** argv;
+	if(strcmp(argv[0], ""))
+}
+
+int main(){
+
+	return 0;
+}
